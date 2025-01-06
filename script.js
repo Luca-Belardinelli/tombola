70fc3495a5eb031537a2b8a650c360c198873812
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-06 19:59:27 +0100

    [js]

---
+++ script.js
@@ -0,0 +1,36 @@
+// // tombola
+
+// // creo un array da 1 a 90
+// let numbers = [];
+
+//  for ( let i = 1 ; i <= 90; i++) {
+
+//  numbers.push(i);
+
+// }
+
+// // creo un array per i numeri estratti
+
+// let extraction = [];
+
+// // estrazione numeri
+
+//  for ( let i = 1 ; i <= 90; i++) {
+
+// // numeri disponibili
+
+//  console.log(`Numeri disponibili: ${numbers}`);
+
+// // conferma
+
+//  confirm("Estrai un numero");
+
+// // estrazione 
+// let calledIndex = Math.floor(Math.random() * numbers.length);
+// let called = extraction[calledIndex];
+
+// console.log(`Numero estratto: ${i}`);
+
+// }
+
+
